70ccbc2695ec827ae745a22dda11ad30ef0d2afa
Jake Brändström <norojake@gmail.com>
2023-02-14 15:48:28 +0100

    Imported unity project

---
+++ Assets/Script/Eagle.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle : Enemy
+{
+
+}
+
+// Platformer 29: Troubleshoot Enemy
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    //Start() variables
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+
+    //FSM
+    private enum State { idle, running, jumping, falling, hurt }
+    private State state = State.idle;
+
+    //Inspector variables
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int cherries = 0;
+    [SerializeField] private Text cherryText;
+    [SerializeField] private float hurtForce = 10f;
+    [SerializeField] private AudioSource cherry;
+    [SerializeField] private AudioSource footstep;
+    [SerializeField] private int health;
+    [SerializeField] private Text healthAmount;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+        healthAmount.text = health.ToString();
+    }
+
+    private void Update()
+    {
+        if (state != State.hurt)
+        {
+            Movement();
+        }
+        AnimationState();
+        anim.SetInteger("state", (int)state); //sets animation based on Enumerator state
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Collectable")
+        {
+            cherry.Play();
+            Destroy(collision.gameObject);
+            cherries += 1;
+            cherryText.text = cherries.ToString();
+        }
+        if (collision.tag == "Powerup")
+        {
+            Destroy(collision.gameObject);
+            jumpForce = 25f;
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+
+            if (state == State.falling)
+            {
+                enemy.JumpedOn();
+                //Jump(); - have no function for this
+            }
+            else
+            {
+                state = State.hurt;
+               // HandleHealth();
+                health -= 1;
+                healthAmount.text = health.ToString();
+                if(health <= 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+
+                if (other.gameObject.transform.position.x > transform.position.x)
+                {
+                    //Enemy is to the right therefore damaged and move left
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    //Enemy to the left therefore damaged and move right
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+        }
+    }
+
+  // private void HandleHealth()
+
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+
+        //Moving left
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+            //anim.SetBool("running", true)
+        }
+        //Moving right
+
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+
+
+    private void AnimationState() //Movement 2
+    {
+        if (state == State.jumping)
+        {
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if (state == State.falling)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt)
+        {
+            if (Mathf.Abs(rb.velocity.x) < .1f)
+            {
+                state = State.idle;
+            }
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
+
+
+    private void Footstep()
+    {
+        footstep.Play();
+    }
+}
+
+
+
+
+
+//Troubleshoot Enemy TIME : 3.00
+
